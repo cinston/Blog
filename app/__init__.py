55e9ef736f935e7a9a9915dcfcfedb1ef5ec5f2b
Winston <wagawc@gmail.com>
2019-02-18 17:47:50 +0300

    import login manager from flask login and an instance of the class

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,14 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_uploads import UploadSet, configure_uploads
 
+
 bootstrap = Bootstrap()
 db = SQLAlchemy
 mail = Mail
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
